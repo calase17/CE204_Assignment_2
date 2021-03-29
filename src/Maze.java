2602224a9c2188594adade1999211c7a658deeea
Calase_17 <kamerong111@gmail.com>
2021-03-29 22:45:05 +0100

    done

--- src/Maze.java
+++ src/Maze.java
@@ -1,19 +1,44 @@
-import java.util.Arrays;
+import com.sun.source.tree.WhileLoopTree;
+
+import java.util.*;
 
 public class Maze {
     int width;
     int height;
-    int [][] array;
+    int [] array;
     MatrixGraph matrixGraphG;
     MatrixGraph spanningTreeT;
 
+    class Edge implements Comparable<Edge>{
+        int x;
+        int y;
+        double weight;
+
+        Edge(int x,int y,double weight){
+            this.x = x;
+            this.y = y;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return (int) (this.weight - o.weight);
+        }
+
+        @Override
+        public String toString() {
+            return "x:" + x + " y:" + y + " weight:" + weight;
+        }
+    }
+
     public Maze(int width, int height){
+
         this.height = height;
         this.width = width;
         matrixGraphG = new MatrixGraph(this.height * this.width, false);
         for (int i = 0; i < matrixGraphG.numVertices(); i++){
-            double randWeight = Math.random() * 20.0;
-            double randWeight2 = Math.random() * 20.0;
+            double randWeight = Math.random() * 50;
+            double randWeight2 = Math.random() * 50;
             if (i+1 < 100 && i+10 < 100){
                 matrixGraphG.addEdge(i,i+1, randWeight);
                 matrixGraphG.addEdge(i,i+10,randWeight2);
@@ -24,63 +49,77 @@ public class Maze {
     public void print(){
         int start = (int) (Math.random() * 10);
         int end = (int) (Math.random() * 10);
-        int countX = 0;
-        int countY = 0;
-        for (int i = 0; i < matrixGraphG.numVertices(); i++ ){
-            if ((countX == 0 && countY == start) || (countX == 9 && countY == end)){
-                System.out.print("*");
+        int vertexIndex = 0;
+        for (int y = 0; y < height; y++){
+            for (int x = 0; x < width; x++){
+                if ((x == 0 && y == start) || (x == 9 && y == end)){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print("+");
+                }
+                if (x < width-1 && spanningTreeT.isEdge(vertexIndex, vertexIndex+1)){
+                    System.out.print("-");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                vertexIndex++;
+            }
+            System.out.println();
+            if (y < height-1 && spanningTreeT.isEdge(vertexIndex, vertexIndex+10)){
+                System.out.println("|");
             }
             else{
-                System.out.print("+");
+                System.out.println(" ");
             }
-            if (countX < 9 && spanningTreeT.isEdge(i, i+1)){
-                System.out.print("-");
-            }
-            if (countX == 0 && spanningTreeT.isEdge(i,  i+10)){
-                System.out.print("|");
-            }
-            if (countX == 9){
-                countX = 0;
-                countY++;
-            }
-            countX++;
         }
     }
-
 
 
     public void initializeComponents(){
-        array = new int[matrixGraphG.numVertices()][matrixGraphG.numVertices()];
-    }
-
-    public void spanningTree(){
-        initializeComponents();
-        spanningTreeT = new MatrixGraph(width * height, false);
+        array = new int[matrixGraphG.numVertices()];
         for (int component = 0; component < matrixGraphG.numVertices(); component++){
-            int [] neighbours = matrixGraphG.neighbours(component);
-            int smallestNeighbour = 0;
-            double smallestWeight = 0;
-            for (int i = 0; i < neighbours.length; i++ ){
-                if (i == 0){
-                    smallestNeighbour = neighbours[i];
-                }
-                double weight = matrixGraphG.weight(component, neighbours[i]);
-                if (weight < smallestWeight){
-                    smallestWeight = weight;
-                    smallestNeighbour = neighbours[i];
-                }
-            }
-            spanningTreeT.addEdge(component, smallestNeighbour, smallestWeight);
-            mergeComponents(component, smallestNeighbour);
+            array[component] = component;
         }
     }
 
-    public void mergeComponents(int x, int y){
-        array = Arrays.copyOf(array, array[x].length + 1);
-        array[x][array[x].length - 1] = y;
+
+
+
+    public void spanningTree(){
+        initializeComponents();
+        spanningTreeT = new MatrixGraph(matrixGraphG.numVertices(), false);
+
+            for (int component = 0; component < matrixGraphG.numVertices(); component++){
+                int [] neighbours = matrixGraphG.neighbours(component);
+                TreeSet<Edge> edgeSet = new TreeSet<>();
+                for (int neighbour : neighbours){
+                    if (array[component] != array[neighbour]){
+                        double weight = matrixGraphG.weight(component, neighbour);
+                        Edge edge = new Edge(component, neighbour, weight);
+                        edgeSet.add(edge);
+                    }
+                }
+                if (edgeSet.isEmpty()) {
+                    break;
+                }
+                Edge cheapestEdge = edgeSet.first();
+                if (!spanningTreeT.isEdge(cheapestEdge.x, cheapestEdge.y)){
+                    spanningTreeT.addEdge(cheapestEdge.x, cheapestEdge.y, cheapestEdge.weight);
+                    mergeComponents(component, cheapestEdge.y);
+                }
+        }
     }
 
 
+    public void mergeComponents(int x, int y){
+        for (int i = 0; i < array.length; i++){
+            if (array[i] == array[y]){
+                array[i] = array[x];
+            }
+        }
+    }
 
 
 
